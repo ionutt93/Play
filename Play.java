47ff384c81dcaa58df47d1af52a11fe6911ecdac
Ioan Troana <ionutt_93@yahoo.com>
2013-02-26 20:35:05 +0000

    branch testing

--- Play.java
+++ Play.java
@@ -3,6 +3,7 @@ class Play
 	public static void main(String[] args)
 	{
 		System.out.println("branching2");
-		System.out.println("branching");		
+		System.out.println("branching");
+		System.out.println("This is the true branch");		
 	}
 }
