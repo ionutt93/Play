208b28cb6e2cf905d9ab5fd9f1a6774125883fd3
Ioan Troana <ionutt_93@yahoo.com>
2013-02-26 23:30:14 +0000

    starting to test graphics

--- Play.java
+++ Play.java
@@ -1,19 +1,4 @@
 class Play
 {
-	public Play()
-	{
-
-	}
-
-	private void test()
-	{
-		for(int i = 1; i < 100; i++)
-			System.out.println("tested");
-	}
-
-	public static void main(String[] args)
-	{
-		Play p = new Play();
-		p.test();
-	}
+	
 }
