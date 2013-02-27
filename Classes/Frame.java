9f2de31c58607b227b386322a2460080dfb4a2c5
Ioan Troana <ionutt_93@yahoo.com>
2013-02-27 16:21:12 +0000

    first commit from eclipse

---
+++ Classes/Frame.java
@@ -0,0 +1,16 @@
+package Classes;
+import javax.swing.*;
+
+public class Frame extends JFrame {
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+	}
+	
+	public void init() {
+		
+	}
+	
+	public Frame() {
+		
+	}
+}
