4c8936eea149924d9e3a4d43c9ecf70a1a84a9f2
Ioan Troana <ionutt_93@yahoo.com>
2013-02-27 16:41:34 +0000

    started the frame class

--- Classes/Frame.java
+++ Classes/Frame.java
@@ -1,16 +1,30 @@
 package Classes;
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JFrame {
+	private String title;
+	private Dimension size;
+	
+	public Frame() {
+		title = "Tower Defence v1";
+		size = new Dimension(500, 600);
+		
+		setSize(size);
+		setTitle(title);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		init();
+	}
+	
+	public void init() {
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
 		Frame frame = new Frame();
 	}
 	
-	public void init() {
-		
-	}
 	
-	public Frame() {
-		
-	}
 }
